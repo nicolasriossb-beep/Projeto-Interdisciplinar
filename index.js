546b3434e8d202682401fe9402f955065045202e
otaviocastro515-blip <otaviocastro515@gmail.com>
2026-04-27 10:33:42 -0300

    Alterações 27/04/2026
    Updated toggleMenu function to target btnConfig and added WhatsApp button.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function toggleMenu() {
-    document.getElementById("menuConfig").classList.toggle("hidden");
+    const menu = document.getElementById("btnConfig");
+    menu.classList.toggle("hidden");
 }
 
 function toggleDarkMode() {
     document.body.classList.toggle("dark");
 }
+
+document.getElementById("btnConfig").addEventListener("click", toggleMenu);
+
+//https://wa.me/5511945070187
+
+const botao = document.createElement("a");
+
+botao.href = "https://wa.me/5511945070187?";
+botao.target = "_blank";
+botao.classList.add("whatsapp-btn");
+
+botao.innerHTML= "💬";
+
+document.body.appendChild(botao);
