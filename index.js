0955b0d8827fa9c3edfcfdf9da4d3244ff90b558
Otávio <otaviocastro515@gmail.com>
2026-04-29 09:35:12 -0300

    Alterações - 29/04/2026

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//Alterar tema
+
 function toggleMenu() {
     const menu = document.getElementById("btnConfig");
     menu.classList.toggle("hidden");
@@ -6,17 +8,45 @@ function toggleMenu() {
 function toggleDarkMode() {
     document.body.classList.toggle("dark");
 }
-
 document.getElementById("btnConfig").addEventListener("click", toggleMenu);
 
 //https://wa.me/5511945070187
 
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+//Botão do Whatsapp
+
 const botao = document.createElement("a");
 
-botao.href = "https://wa.me/5511945070187?";
+botao.href = "https://wa.me/5511945070187?=Olá,%20vim%20pelo%20site";
 botao.target = "_blank";
 botao.classList.add("whatsapp-btn");
 
-botao.innerHTML= "💬";
-
+botao.innerHTML= '<img src="https://cdn-icons-png.flaticon.com/512/733/733585.png" alt=WhatsApp">';
 document.body.appendChild(botao);
+
+const botao3 = document.createElement("a");
+
+botao3.href = "https://wa.me/5511945070187?=Olá,%20vim%20pelo%20site";
+botao3.target = "_blank";
+botao3.classList.add("whatsapp-btn2");
+
+botao3.innerHTML= '<img src="https://cdn-icons-png.flaticon.com/512/733/733585.png" alt=WhatsApp">';
+document.body.appendChild(botao3);
+
+
+    sidebar.appendChild(botao2);
+
+});
+/*Botao de saida*/
+function fazerLogout(){
+    localStorage.clear();
+    sessionStorage.clear();
+
+    window.location.href="chrome://newtab";
+
+  
+
+}
